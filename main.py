d4b003394dd38aa228a6834a1833031905e6f951
Squidfishrl <ivo.genchev4@gmail.com>
2021-10-23 16:04:47 +0300

    Add rows and columns
    Also fix an iteration bug -> used to not iterate the last element

--- main.py
+++ main.py
@@ -11,12 +11,26 @@ class Cell:
 
 
 class Table:
-    def __init__(self, rows: int, columns: int):
+    def __init__(self, rows: int=0, columns: int=0, def_value = ' '):
         self.columns = columns  # x
         self.rows = rows  # y
 
         # [row][column]
-        self.cell_matrix = [ [Cell(column, row) for column in range(0, columns)] for row in range(0, rows) ]
+        self.cell_matrix = [ [Cell(column, row, def_value) for column in range(0, columns)] for row in range(0, rows) ]
+
+    def add_row(self, def_value = ' '):
+        """Adds a row at the end of the table."""
+
+        self.rows += 1
+        self.cell_matrix.append([Cell(column, self.rows-1, def_value) for column in range(0, self.columns)])
+
+    def add_column(self, def_value = ' '):
+        """Adds a column at the end of all rows."""
+
+        self.columns += 1
+
+        for row in range(0, self.rows):
+            self.cell_matrix[row].append(Cell(self.columns-1, row, def_value))
 
     def __len__(self):
         return self.rows * self.columns
@@ -28,7 +42,7 @@ class Table:
     
     def __next__(self) -> Cell:
 
-        if self.row == self.rows-1 and self.column == self.columns-1:
+        if self.row == self.rows:
             raise StopIteration
 
         x = self.column
@@ -41,3 +55,15 @@ class Table:
             self.column += 1
 
         return self.cell_matrix[y][x]
+
+
+if __name__ == '__main__':
+    table = Table(2,3, "hello")
+    for cell in table:
+        print(cell)
+
+    table.add_column("World")
+
+    print("\n")
+    for cell in table:
+        print(cell)
